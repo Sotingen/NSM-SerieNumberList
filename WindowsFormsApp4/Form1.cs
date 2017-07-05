4b9799554a43ff4031262692b43af63d70f4fba8
Sotingen <Martin.markussen92@gmail.com>
2017-07-05 10:05:52 +0200

    Test request

--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp4
     public partial class Form1 : Form
     {
         //Fix this!
+        //Is it fixed?!??!?
         String cons;
         String unitUpdate;
         String unitFill = @"SELECT DISTINCT Unit FROM InvList";
